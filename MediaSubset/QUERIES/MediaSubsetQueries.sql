5a00ad393f82f4914e6c1d786f50c20a6b3ac22d
Brent Buchholz <brentbuchholz@gmail.com>
2017-01-08 18:57:34 -0600

    working on media queries

--- MediaSubset/QUERIES/MediaSubsetQueries.sql
+++ MediaSubset/QUERIES/MediaSubsetQueries.sql
@@ -23,3 +23,29 @@ FROM MediaDevice;
 SELECT MediaRootId, MediaRootPath
 FROM MediaRoot
 WHERE MediaDeviceId = ? ;
+
+-- INSERT_PATH
+INSERT OR IGNORE INTO MediaPath
+	(MediaPathValue)
+VALUES
+	(?);
+
+-- INSERT_MEDIA_FILE_NAME
+INSERT OR IGNORE INTO Media
+	(MediaFileName)
+VALUES
+	(?);
+
+-- INSERT_MEDIA_DEVICE_PATH_FILENAME_DEVICEID_PATH_X_Y_Z
+INSERT OR IGNORE INTO MediaDevicePath
+	(MediaId, MediaDeviceId, MediaPathId)
+VALUES
+	(
+		(SELECT MediaId FROM Media WHERE MediaFileName = ? AND MediaHash IS NULL LIMIT 1),
+		?,
+		(SELECT MediaPathId FROM MediaPath WHERE MediaPathValue = ? LIMIT 1)
+	)
+
+
+-- SELECT_PATH_FOR_DEVICE_ID_LIKE_ROOT_PATH_X_Y
+SELECT -- IN PROGRESS	
--- MediaSubset/QUERIES/NwdContract-MediaSubsetQueries.sql
+++ MediaSubset/QUERIES/NwdContract-MediaSubsetQueries.sql
@@ -25,3 +25,25 @@
 		  + NwdContract.COLUMN_MEDIA_ROOT_PATH + " "
 + "FROM " + NwdContract.TABLE_MEDIA_ROOT + " " 
 + "WHERE " + NwdContract.COLUMN_MEDIA_DEVICE_ID + " = ? ; "
+
+-- INSERT_PATH
+"INSERT OR IGNORE INTO " + NwdContract.TABLE_MEDIA_PATH + " " + 
+"	(" + NwdContract.COLUMN_MEDIA_PATH_VALUE + ") " + 
+"VALUES " + 
+"	(?); "  
+
+-- INSERT_MEDIA_FILE_NAME 
+"INSERT OR IGNORE INTO " + NwdContract.TABLE_MEDIA + " " + 
+"	(" + NwdContract.COLUMN_MEDIA_FILE_NAME + ") " + 
+"VALUES " + 
+"	(?); " 
+
+-- INSERT_MEDIA_DEVICE_PATH_FILENAME_DEVICEID_PATH_X_Y_Z 
+"INSERT OR IGNORE INTO " + NwdContract.TABLE_MEDIA_DEVICE_PATH + " " + 
+"	(" + NwdContract.COLUMN_MEDIA_ID + ", " + NwdContract.COLUMN_MEDIA_DEVICE_ID + ", " + NwdContract.COLUMN_MEDIA_PATH_ID + ") " + 
+"VALUES " + 
+"	( " + 
+"		(SELECT " + NwdContract.COLUMN_MEDIA_ID + " FROM " + NwdContract.TABLE_MEDIA + " WHERE " + NwdContract.COLUMN_MEDIA_FILE_NAME + " = ? AND " + NwdContract.COLUMN_MEDIA_HASH + " IS NULL LIMIT 1), " + 
+"		?, " + 
+"		(SELECT " + NwdContract.COLUMN_MEDIA_PATH_ID + " FROM " + NwdContract.TABLE_MEDIA_PATH + " WHERE " + NwdContract.COLUMN_MEDIA_PATH_VALUE + " = ? LIMIT 1) " + 
+"	) " 

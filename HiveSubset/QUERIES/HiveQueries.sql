1f1b3b0742eafcf8558f1b9ae49ae818e924d645
Brent Buchholz <brentbuchholz@gmail.com>
2017-07-15 14:35:09 -0500

    added retrieval query for hive root id

--- HiveSubset/QUERIES/HiveQueries.sql
+++ HiveSubset/QUERIES/HiveQueries.sql
@@ -5,6 +5,11 @@ SELECT HiveRootId,
 	   HiveRootDeactivatedAt
 FROM HiveRoot
 
+-- SELECT_HIVE_ROOT_ID_FOR_NAME_X
+SELECT HiveRootId
+FROM HiveRoot
+WHERE HiveRootName = ? 
+
 -- INSERT_HIVE_ROOT_NAME_X
 INSERT OR IGNORE INTO HiveRoot
 	(HiveRootName)

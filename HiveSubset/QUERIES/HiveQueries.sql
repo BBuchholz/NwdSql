0db9763924e501fcb316623a7d354b216399f8d0
Brent Buchholz <brentbuchholz@gmail.com>
2017-07-15 14:52:55 -0500

    add support for deactivated hive roots

--- HiveSubset/QUERIES/HiveQueries.sql
+++ HiveSubset/QUERIES/HiveQueries.sql
@@ -4,6 +4,15 @@ SELECT HiveRootId,
 	   HiveRootActivatedAt,
 	   HiveRootDeactivatedAt
 FROM HiveRoot
+WHERE IFNULL(HiveRootActivatedAt, '') >= IFNULL(HiveRootDeactivatedAt, '')
+
+-- SELECT_DEACTIVATED_HIVE_ROOTS
+SELECT HiveRootId,
+       HiveRootName,
+	   HiveRootActivatedAt,
+	   HiveRootDeactivatedAt
+FROM HiveRoot
+WHERE IFNULL(HiveRootActivatedAt, '') < IFNULL(HiveRootDeactivatedAt, '')
 
 -- SELECT_HIVE_ROOT_ID_FOR_NAME_X
 SELECT HiveRootId
